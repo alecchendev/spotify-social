76f45cf666a3d18495fea999dcbc4ec663c4525d
Alec Chen <alecchendev@gmail.com>
2021-04-03 12:55:17 -0500

    Set up tabs and buttons on account page

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { getFeedData } from '../lib/api.js';
+import styles from '../styles/account.module.css';
 import Button from '../components/button.js';
 import Heading from '../components/heading.js';
 import Kicker from '../components/kicker.js';
@@ -8,6 +9,7 @@ import Kicker from '../components/kicker.js';
 export default function Account() {
 
 	const [ feedData, setFeedData ] = React.useState({});
+	const [ tab, setTab ] = React.useState('feed'); // feed, preferences
 
 	const { id } = useParams();
 
@@ -28,16 +30,24 @@ export default function Account() {
 
 	}, []);
 
+	const switchTab = (newTab) => {
+		setTab(newTab);
+	};
+
 
 	return (
 		<div>
 			<Kicker>Account</Kicker>
-			<Heading>Preferences/Feed</Heading>
-			{JSON.stringify(feedData)}
-			<br/>
-			{id}
-			<br/>
-			<Link to={'/' + id}><Button>View Profile</Button></Link>
+			<div className={styles.headingBox}>
+				<Heading className={styles.heading}>{tab}</Heading>
+				<div className={styles.vertAlign}>
+					<Link to={'/' + id}><Button>View Profile</Button></Link>
+				</div>
+			</div>
+			<div className={styles.tabBox}>
+				<button className={styles.tabButton + ' ' + (tab === 'feed' ? styles.tabButtonTrue : styles.tabButtonFalse)} onClick={() => switchTab('feed')}>Feed</button>
+				<button className={styles.tabButton + ' ' + (tab === 'preferences' ? styles.tabButtonTrue : styles.tabButtonFalse)} onClick={() => switchTab('preferences')}>Preferences</button>
+			</div>
 		</div>
 	)
 }
