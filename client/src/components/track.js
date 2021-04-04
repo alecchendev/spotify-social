184d31caedd09b58061f2ef6e98f79e2100bc59a
Alec Chen <alecchendev@gmail.com>
2021-04-04 13:24:59 -0500

    Play preview song on profile

--- client/src/components/track.js
+++ client/src/components/track.js
@@ -1,7 +1,24 @@
+import React from 'react';
 import styles from '../styles/track.module.css';
 import { Text, Subtext } from '.';
 
 export default function Track({ item }) {
+
+	const [ playing, setPlaying ] = React.useState(false);
+
+	const playPause = () => {
+		console.log(playing);
+		if (item.preview_url) {
+			if (playing) {
+				document.getElementById(item.preview_url).pause();
+				setPlaying(false);
+			} else {
+				document.getElementById(item.preview_url).play();
+				setPlaying(true);
+			}
+		}
+	}
+
 	return (<div className={styles.track}>
 		<div className={styles.albumBox}>
 		{
@@ -15,8 +32,9 @@ export default function Track({ item }) {
 				item
 				?
 				<div>
-					<Text>{item.name}</Text>
+					<span className={item.preview_url && styles.songTitle}><Text onClick={() => playPause()}>{item.name}</Text></span>
 					<Subtext>{item.artists.map(artist => artist.name).join(', ')}</Subtext>
+					<audio id={item.preview_url} className={styles.player} src={item.preview_url} controls></audio>
 				</div>
 				:
 				<Text>Playing advertisment... refresh in a little.</Text>
