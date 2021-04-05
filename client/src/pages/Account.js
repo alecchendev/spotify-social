55e902317d6a1759c304b66f6d3260d333f581f1
Alec Chen <alecchendev@gmail.com>
2021-04-05 17:01:04 -0500

    Revert "Adapt api file structure for serverless on vercel"
    This reverts commit e5da8b47dfc8c80f553ecb744b80acef114b4762.

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -5,8 +5,7 @@ import styles from '../styles/account.module.css';
 import utilStyles from '../styles/utils.module.css';
 import { Text, Button, Heading, Kicker, Feed, Settings } from '../components';
 
-// const url = process.env.NODE_ENV === 'production' ? 'https://my-spotify-social.herokuapp.com' : 'http://localhost:5000';
-const url = process.env.NODE_ENV === 'production' ? 'https://spotifysocial.me/api' : 'http://localhost:5000';
+const url = process.env.NODE_ENV === 'production' ? 'https://my-spotify-social.herokuapp.com' : 'http://localhost:5000';
 const API_VERSION = 'v1'; // TEMPORARY FIX LATE
 
 export default function Account() {
