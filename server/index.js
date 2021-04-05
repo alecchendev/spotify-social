beafe14175579cb4dfadcf80ff9df4650278859b
Alec Chen <30279834+alecchendev@users.noreply.github.com>
2021-04-05 15:15:27 -0500

    💻 Adapt links for CDN deployment (#14)
    * Change production redirect frontendUrl to spotifysocial.me
    
    * Have backend redirect to frontend url for frontend routes
    
    * Allow new frontend url in cors policy

--- server/index.js
+++ server/index.js
@@ -7,23 +7,28 @@ const api = require('./' + process.env.API_VERSION);
 
 const app = express();
 
+const frontendUrl = process.env.NODE_ENV === 'production' ? 'https://www.spotifysocial.me' : 'http://localhost:3000';
+
 // app.use(cors());
 app.use(cors({
   origin: [
     'http://localhost:3000', // frontend
-    'http://my-spotify-social.herokuapp.com'
+    'http://my-spotify-social.herokuapp.com',
+    'https://www.spotifysocial.me'
   ],
   credentials: true // for cookies
 }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/' + process.env.API_VERSION, api);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.redirect(frontendUrl + req.url);
 });
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 const port = process.env.PORT || 5000 || '0.0.0.0';
 app.listen(port);
