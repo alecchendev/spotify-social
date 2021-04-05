9b76f768c2d2656dbc2ad840d68b5e7f84091e31
Alec Chen <alecchendev@gmail.com>
2021-04-05 14:22:55 -0500

    Have backend redirect to frontend url for frontend routes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const api = require('./' + process.env.API_VERSION);
 
 const app = express();
 
+const frontendUrl = process.env.NODE_ENV === 'production' ? 'https://www.spotifysocial.me' : 'http://localhost:3000';
+
 // app.use(cors());
 app.use(cors({
   origin: [
@@ -17,13 +19,15 @@ app.use(cors({
 }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/' + process.env.API_VERSION, api);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.redirect(frontendUrl + req.url);
 });
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 const port = process.env.PORT || 5000 || '0.0.0.0';
 app.listen(port);
