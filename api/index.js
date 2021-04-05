a4baf868ebc88febe61aeb98a33a50d267abfd40
Alec Chen <alecchendev@gmail.com>
2021-04-05 16:42:57 -0500

    Implement test routes in index.js

--- api/index.js
+++ api/index.js
@@ -21,16 +21,20 @@ app.use(cors({
 app.use(cookieParser());
 
 
-app.use('/' + process.env.API_VERSION, (req, res, next) => {
+app.use('/', (req, res) => {
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  next();
-}, api);
+  res.send({
+    message: 'It worked!'
+  })
+});
+
+app.use('/v1', api);
 
 app.get('*', (req, res) => {
   // res.sendFile(path.join(__dirname, '../client/build/index.html'));
   res.redirect(frontendUrl + req.url);
 });
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
 module.exports = app;
