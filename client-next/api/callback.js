83c0ef9c43e902e607980b0fdd57e0216e001948
Alec Chen <alecchendev@gmail.com>
2021-04-07 17:26:44 -0500

    Use set header and cookie package instead of res.cookie from express

--- client-next/api/callback.js
+++ client-next/api/callback.js
@@ -1,13 +1,20 @@
 
 // Login/auth callback
 
+const cookie = require('cookie');
+
 module.exports = (req, res) => {
 	const { jwtToken, id } = req.query;
 
-  res.cookie('jwtToken', jwtToken, {
-  	expires: new Date(Date.now() + (1000 * 60 * 30)),
-  	httpOnly: true
-  });
+  // res.cookie('jwtToken', jwtToken, {
+  // 	expires: new Date(Date.now() + (1000 * 60 * 30)),
+  // 	httpOnly: true
+  // });
+
+  res.setHeader('Set-Cookie', cookie.serialize('jwtToken', jwtToken, {
+    httpOnly: true,
+    maxAge: 60 * 60 * 24 * 7 // 1 week
+  }));
 
   const fullUrl = req.protocol + '://' + req.get('host');
 
--- client-next/api/test.js
+++ client-next/api/test.js
@@ -1,10 +1,6 @@
 
 module.exports = (req, res) => {
 
-	res.cookie('cookieTest', 'hello', {
-		httpOnly: true
-	}),
-
 	res.send({
 		sanityCheck: 'sane'
 	});
