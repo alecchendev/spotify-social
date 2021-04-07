3d35a6afcd38d098536b40dc6fd0a876932ed3c5
Alec Chen <alecchendev@gmail.com>
2021-04-06 21:30:44 -0500

    Enable all cors

--- server/index.js
+++ server/index.js
@@ -7,15 +7,15 @@ const api = require('./' + process.env.API_VERSION);
 
 const app = express();
 
-// app.use(cors());
-app.use(cors({
-  origin: [
-    'http://localhost:3000', // frontend
-    'http://my-spotify-social.herokuapp.com',
-    'https://www.spotifysocial.me'
-  ],
-  credentials: true // for cookies
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: [
+//     'http://localhost:3000', // frontend
+//     'http://my-spotify-social.herokuapp.com',
+//     'https://www.spotifysocial.me'
+//   ],
+//   credentials: true // for cookies
+// }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
