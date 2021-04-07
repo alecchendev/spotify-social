280d6b94bdc4fccdd1e2c3d17769b6f0cae0d2c3
Alec Chen <alecchendev@gmail.com>
2021-04-07 16:59:20 -0500

    Get rid of setting cookies in callback.js

--- client-next/pages/api/callback.js
+++ client-next/pages/api/callback.js
@@ -4,18 +4,13 @@ module.exports = (req, res) => {
 
 	const { jwtToken, id } = req.query;
 
-  res.cookie('jwtToken', jwtToken, {
-  	expires: new Date(Date.now() + (1000 * 60 * 30)),
-  	httpOnly: true
-  });
+  // res.cookie('jwtToken', jwtToken, {
+  // 	expires: new Date(Date.now() + (1000 * 60 * 30)),
+  // 	httpOnly: true
+  // });
 
   const fullUrl = req.protocol + '://' + req.get('host');
 
-  // res.redirect(fullUrl + '/account/' + id);
-  res.send({
-    jwtToken,
-    id,
-    fullUrl
-  })
+  res.redirect(fullUrl + '/account/' + id);
 
 };
